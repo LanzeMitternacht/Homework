c55de58061557ca8f866a4907cc40218bbaacb25
LanzeMitternacht <lancemitternacht@gmail.com>
2024-01-15 08:50:13 +0300

    Adding new code file

---
+++ C#/WindowsForms/Explorer/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TreeDisk = new System.Windows.Forms.TreeView();
+            this.TBAddress = new System.Windows.Forms.TextBox();
+            this.LVFile = new System.Windows.Forms.ListView();
+            this.BBack = new System.Windows.Forms.Button();
+            this.BForward = new System.Windows.Forms.Button();
+            this.ConMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.открытьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenConStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitConStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ReloadStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ConMenuStrip.SuspendLayout();
+            this.MenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TreeDisk
+            // 
+            this.TreeDisk.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.TreeDisk.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TreeDisk.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TreeDisk.Location = new System.Drawing.Point(12, 89);
+            this.TreeDisk.MinimumSize = new System.Drawing.Size(251, 433);
+            this.TreeDisk.Name = "TreeDisk";
+            this.TreeDisk.Size = new System.Drawing.Size(251, 447);
+            this.TreeDisk.TabIndex = 0;
+            this.TreeDisk.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.TreeDisk_AfterExpand);
+            this.TreeDisk.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.TreeDisk_NodeMouseDoubleClick);
+            // 
+            // TBAddress
+            // 
+            this.TBAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBAddress.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TBAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TBAddress.Location = new System.Drawing.Point(129, 43);
+            this.TBAddress.Name = "TBAddress";
+            this.TBAddress.Size = new System.Drawing.Size(771, 26);
+            this.TBAddress.TabIndex = 1;
+            this.TBAddress.TextChanged += new System.EventHandler(this.TBAddress_TextChanged);
+            // 
+            // LVFile
+            // 
+            this.LVFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LVFile.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.LVFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LVFile.HideSelection = false;
+            this.LVFile.Location = new System.Drawing.Point(278, 89);
+            this.LVFile.Name = "LVFile";
+            this.LVFile.Size = new System.Drawing.Size(622, 447);
+            this.LVFile.TabIndex = 2;
+            this.LVFile.UseCompatibleStateImageBehavior = false;
+            this.LVFile.ItemActivate += new System.EventHandler(this.LVFile_ItemActivate);
+            // 
+            // BBack
+            // 
+            this.BBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BBack.Location = new System.Drawing.Point(12, 40);
+            this.BBack.Name = "BBack";
+            this.BBack.Size = new System.Drawing.Size(41, 35);
+            this.BBack.TabIndex = 3;
+            this.BBack.Text = "←";
+            this.BBack.UseVisualStyleBackColor = true;
+            this.BBack.Click += new System.EventHandler(this.BBack_Click);
+            // 
+            // BForward
+            // 
+            this.BForward.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BForward.Location = new System.Drawing.Point(64, 40);
+            this.BForward.Name = "BForward";
+            this.BForward.Size = new System.Drawing.Size(41, 35);
+            this.BForward.TabIndex = 4;
+            this.BForward.Text = "→";
+            this.BForward.UseVisualStyleBackColor = true;
+            this.BForward.Click += new System.EventHandler(this.BForward_Click);
+            // 
+            // ConMenuStrip
+            // 
+            this.ConMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem1,
+            this.закрытьToolStripMenuItem1});
+            this.ConMenuStrip.Name = "ConMenuStrip";
+            this.ConMenuStrip.Size = new System.Drawing.Size(122, 48);
+            // 
+            // открытьToolStripMenuItem1
+            // 
+            this.открытьToolStripMenuItem1.Name = "открытьToolStripMenuItem1";
+            this.открытьToolStripMenuItem1.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem1.Text = "Открыть";
+            // 
+            // закрытьToolStripMenuItem1
+            // 
+            this.закрытьToolStripMenuItem1.Name = "закрытьToolStripMenuItem1";
+            this.закрытьToolStripMenuItem1.Size = new System.Drawing.Size(121, 22);
+            this.закрытьToolStripMenuItem1.Text = "Закрыть";
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(922, 24);
+            this.MenuStrip.TabIndex = 6;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenConStripMenuItem,
+            this.ExitConStripMenuItem,
+            this.ReloadStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // OpenConStripMenuItem
+            // 
+            this.OpenConStripMenuItem.Name = "OpenConStripMenuItem";
+            this.OpenConStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.OpenConStripMenuItem.Text = "Открыть";
+            this.OpenConStripMenuItem.Click += new System.EventHandler(this.OpenConStripMenuItem_Click);
+            // 
+            // ExitConStripMenuItem
+            // 
+            this.ExitConStripMenuItem.Name = "ExitConStripMenuItem";
+            this.ExitConStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.ExitConStripMenuItem.Text = "Закрыть";
+            this.ExitConStripMenuItem.Click += new System.EventHandler(this.ExitConStripMenuItem_Click);
+            // 
+            // ReloadStripMenuItem
+            // 
+            this.ReloadStripMenuItem.Name = "ReloadStripMenuItem";
+            this.ReloadStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.ReloadStripMenuItem.Text = "Обновить";
+            this.ReloadStripMenuItem.Click += new System.EventHandler(this.RefreshStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 558);
+            this.Controls.Add(this.MenuStrip);
+            this.Controls.Add(this.BForward);
+            this.Controls.Add(this.BBack);
+            this.Controls.Add(this.LVFile);
+            this.Controls.Add(this.TBAddress);
+            this.Controls.Add(this.TreeDisk);
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ConMenuStrip.ResumeLayout(false);
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TreeDisk;
+        private System.Windows.Forms.TextBox TBAddress;
+        private System.Windows.Forms.ListView LVFile;
+        private System.Windows.Forms.Button BBack;
+        private System.Windows.Forms.Button BForward;
+        private System.Windows.Forms.ContextMenuStrip ConMenuStrip;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenConStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitConStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ReloadStripMenuItem;
+    }
+}
+
---
+++ C#/WindowsForms/Explorer/Form1.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Explorer
+{
+    public partial class Form1 : Form
+    {
+        private Stack<string> folderHistory = new Stack<string>();
+        private string currentFolder;
+        private string clipboardPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTreeView();
+            InitializeListView();
+            InitializeContextMenu();
+            InitializeAddressBar();
+            currentFolder = DriveInfo.GetDrives().FirstOrDefault()?.Name;
+            folderHistory.Push(currentFolder);
+        }
+
+        private void InitializeTreeView()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                TreeNode driveNode = CreateDriveNode(drive);
+                TreeDisk.Nodes.Add(driveNode);
+            }
+        }
+        private void InitializeListView()
+        {
+            LVFile.View = View.Details;
+            LVFile.Columns.Add("Имя", 200);
+            LVFile.Columns.Add("Размер", 100);
+            LVFile.Columns.Add("Тип", 100);
+        }
+        private void InitializeContextMenu()
+        {
+            MenuItem openMenuItem = new MenuItem("Открыть", OpenMenuItem_Click);
+            MenuItem exitMenuItem = new MenuItem("Выход", ExitMenuItem_Click);
+            MenuItem refreshButton = new MenuItem("Обновить", RefreshButton_Click);
+
+            MainMenu mainMenu = new MainMenu(new[] { openMenuItem, exitMenuItem, refreshButton});
+            Menu = mainMenu;
+
+            openMenuItem.Shortcut = Shortcut.CtrlO;
+            exitMenuItem.Shortcut = Shortcut.CtrlX;
+            refreshButton.Shortcut = Shortcut.CtrlY;
+        }
+
+        private void InitializeAddressBar()
+        {
+            TBAddress.TextChanged += TBAddress_TextChanged;
+
+            Controls.Add(TBAddress);
+        }
+
+        private TreeNode CreateDriveNode(DriveInfo drive)
+        {
+            TreeNode driveNode = new TreeNode(drive.Name);
+            driveNode.Tag = drive.Name;
+            driveNode.Nodes.Add("Загрузка");
+
+            return driveNode;
+        }
+        private TreeNode CreateDirectoryNode(string dir)
+        {
+            TreeNode node = new TreeNode(Path.GetFileName(dir));
+            node.Tag = dir;
+            node.Nodes.Add("Загрузка");
+
+            return node;
+        }
+        private void PopulateTreeView(TreeNode parentNode)
+        {
+            try
+            {
+                if (Directory.Exists(parentNode.Tag.ToString()))
+                {
+
+                    string[] dirs = Directory.GetDirectories(parentNode.Tag.ToString());
+
+                    foreach (string dir in dirs)
+                    {
+                        TreeNode node = CreateDirectoryNode(dir);
+                        parentNode.Nodes.Add(node);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Нет доступа: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void PopulateListView(string path)
+        {
+            LVFile.Items.Clear();
+
+            try
+            {
+                PopulateItemsFromPath(Directory.GetDirectories(path), "Папка");
+                PopulateItemsFromPath(Directory.GetFiles(path), "Файл");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Нет доступа: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void PopulateItemsFromPath(string[] items, string itemType)
+        {
+            foreach (string itemPath in items)
+            {
+                string itemName = (itemType == "Папка") ? Path.GetFileName(itemPath) : Path.GetFileNameWithoutExtension(itemPath);
+
+                ListViewItem item = new ListViewItem(itemName);
+                item.SubItems.Add((itemType == "Файл") ? new FileInfo(itemPath).Length.ToString() : "");
+                item.SubItems.Add(itemType);
+                item.Tag = itemPath;
+
+                LVFile.Items.Add(item);
+            }
+        }
+        private void TBAddress_TextChanged(object sender, EventArgs e)
+        {
+            string newPath = TBAddress.Text;
+            if (Directory.Exists(newPath))
+            {
+                currentFolder = newPath;
+                folderHistory.Push(currentFolder);
+                LoadFolder(currentFolder);
+            }
+        }
+        private void TreeDisk_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text == "Загрузка")
+            {
+                e.Node.Nodes.Clear();
+                PopulateTreeView(e.Node);
+            }
+        }
+
+        private void TreeDisk_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text == "Загрузка")
+            {
+                e.Node.Nodes.Clear();
+                PopulateTreeView(e.Node);
+            }
+
+            string selectedFolder = e.Node.Tag?.ToString();
+
+            if (!string.IsNullOrEmpty(selectedFolder) && selectedFolder != currentFolder)
+            {
+                currentFolder = selectedFolder;
+                folderHistory.Push(currentFolder);
+                LoadFolder(currentFolder);
+            }
+        }
+       
+        private void LVFile_ItemActivate(object sender, EventArgs e)
+        {
+            if (LVFile.SelectedItems.Count > 0)
+            {
+                string selectedPath = LVFile.SelectedItems[0].Tag.ToString();
+                if (Directory.Exists(selectedPath))
+                {
+                    PopulateListView(selectedPath);
+                    TBAddress.Text = selectedPath;
+                }
+            }
+        }
+        private void LoadFolder(string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                MessageBox.Show("Выбранная папка не существует.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            folderHistory.Push(folderPath);
+            TBAddress.Text = folderPath;
+            PopulateListView(folderPath);
+        }
+
+        private void FolderBrower()
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string selectedFolder = folderDialog.SelectedPath;
+
+                    if (Directory.Exists(selectedFolder))
+                    {
+                        currentFolder = selectedFolder;
+                        folderHistory.Push(currentFolder);
+                        LoadFolder(currentFolder);
+
+                        ExpandNodes(TreeDisk.Nodes, currentFolder);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выбранная папка не существует.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void ExpandNodes(TreeNodeCollection nodes, string folderPath)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                string nodePath = node.Tag?.ToString();
+
+                if (!string.IsNullOrEmpty(nodePath) && folderPath.StartsWith(nodePath, StringComparison.OrdinalIgnoreCase))
+                {
+                    node.Expand();
+                    ExpandNodes(node.Nodes, folderPath);
+                }
+            }
+        }
+
+        private void CopySelectedItem()
+        {
+            if (LVFile.SelectedItems.Count > 0)
+            {
+                clipboardPath = LVFile.SelectedItems[0].Tag.ToString();
+            }
+        }
+        private void PasteItems()
+        {
+            if (!string.IsNullOrEmpty(clipboardPath) && Directory.Exists(currentFolder))
+            {
+                string fileName = Path.GetFileName(clipboardPath);
+                string destinationPath = Path.Combine(currentFolder, fileName);
+
+                int number = 1;
+                while (File.Exists(destinationPath))
+                {
+                    string newFileName = $"{Path.GetFileNameWithoutExtension(fileName)}_{number}{Path.GetExtension(fileName)}";
+                    destinationPath = Path.Combine(currentFolder, newFileName);
+                    number++;
+                }
+
+                File.Copy(clipboardPath, destinationPath);
+                LoadFolder(currentFolder);
+            }
+        }
+
+        private void DeleteSelectedItem()
+        {
+            if (LVFile.SelectedItems.Count > 0)
+            {
+                string selectedPath = LVFile.SelectedItems[0].Tag.ToString();
+
+                DialogResult result = MessageBox.Show($"Вы уверены, что хотите удалить файл:\n{selectedPath}", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        File.Delete(selectedPath);
+                        LoadFolder(currentFolder);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при удалении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.C))
+            {
+                CopySelectedItem();
+                return true;
+            }
+            else if (keyData == (Keys.Control | Keys.V))
+            {
+                PasteItems();
+                return true;
+            }
+            else if(keyData == Keys.Delete) 
+            { 
+                DeleteSelectedItem(); 
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        private void BForward_Click(object sender, EventArgs e)
+        {
+            if (folderHistory.Count > 1)
+            {
+                string nextFolder = folderHistory.Pop();
+                folderHistory.Push(currentFolder);
+                currentFolder = nextFolder;
+                LoadFolder(currentFolder);
+            }
+        }
+
+        private void BBack_Click(object sender, EventArgs e)
+        {
+            if (folderHistory.Count > 1)
+            {
+                folderHistory.Pop();
+                string previousFolder = folderHistory.Pop();
+                currentFolder = previousFolder;
+                LoadFolder(currentFolder);
+            }
+            else { }
+        }
+
+        private void OpenMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrower();
+        }
+        
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void OpenConStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrower();
+        }
+
+        private void ExitConStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            LoadFolder(currentFolder);
+        }
+
+        private void RefreshStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadFolder(currentFolder);
+        }
+    }
+}
